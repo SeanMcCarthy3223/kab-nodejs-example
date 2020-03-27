c8e95b8bce5aac1b27bec8bfb5851d246f620cbe
Sean McCarthy <sean@Seans-MacBook-Pro.local>
2020-03-27 13:24:41 -0500

    Added Jenkins Integration

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Cloud Paks!");
+  res.send("Hello from Cloud Paks with Jenkins!");
 });
  
 module.exports.app = app;
