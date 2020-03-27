b571ef781a78e186b445d867855fe3281af48498
Sean McCarthy <sean@Seans-MacBook-Pro.local>
2020-03-27 15:34:58 -0500

    testing jenkins

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Cloud Paks with Jenkins!");
+  res.send("Hello from Winner's Circle!");
 });
  
 module.exports.app = app;
